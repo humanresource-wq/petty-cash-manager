13f50533d65e9e50bff7eee00927d1f08aeb730b
harsh.ramani <Harsh@xsworld.online>
2026-07-10 15:43:08 +0530

    Removed the unwanted files

--- src/components/Toast.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Toast({ toastState }) {
-  return (
-    <div id="toast" className={toastState.show ? 'show' : ''}>
-      {toastState.msg}
-    </div>
-  );
-}
